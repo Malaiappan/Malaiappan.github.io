7b922c1c5b2745c9ef56b335be748850e3ccf941
Malaiappan <malaiappan.kannan@gmail.com>
2019-01-15 20:54:24 +0100

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   let devices = await navigator.usb.getDevices();
   devices.forEach(device => {
     // Add |device| to the UI.
+	  console.log('device');
 	  console.log(device);
   });
 });
